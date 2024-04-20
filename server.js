c8c758c047584c639cdbc3ec0daab8ffd6cc087d
anuchit <anuchit.nu567@gmail.com>
2024-04-20 12:18:43 +0700

    emit message

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/client", (req, res) => {
   res.sendFile(__dirname + "/client.html");
 });
 
@@ -15,9 +19,10 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-    socket.on('chat message', (msg) => {
-    console.log(msg)
-})
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3000, () => {
